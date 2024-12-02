f3a018c698e5afa760023dbb16246e422217606c
a22301147-RodrigoPereira <160654359+Rodrigo19Silva@users.noreply.github.com>
2024-12-02 12:48:21 +0000

    nextproject

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2024 - Desenvolvido com Next.js</p>
+    </footer>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function page() {
+    return (
+        <>
+            <h1>Bem-vindo à Página dos Programadores</h1>
+            <p>Esta é a página inicial da aplicação.</p>
+        </>
+    );
+}
